6f86d382affd7edc2334d14f763e0d76bf473d81
mayankkumawat <mayankkumawat015@gmail.com>
2021-05-06 16:29:48 +0530

    added table nd fetching data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ForgetPassword from './containers/ForgetPassword';
 import Main from './containers/Main';
 import DataTable from './components/Table.jsx';
 import { useEffect, useState } from 'react';
+import Api from './api/Api';
 
 
 import {
@@ -13,19 +14,22 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import Api from './api/Api';
 
 
 function App() {
-  const [list, setList] = useState({});
+  const [list, setList] = useState([{}]);
 
   useEffect(() => {
-    setTimeout(async () => {
-      let result = await Api.get("/employees");
-      setList(result.data);
-    }, 1000);
+    const getList = async () => {
+      try {
+        const res = await Api.get("/employees");
+        setList(res.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getList();
   }, []);
-
 
   return (
     <Router>
@@ -33,7 +37,7 @@ function App() {
       <Link to="/"></Link>
       <Switch>
         <Route exact path="/">
-          <DataTable list={list} />
+          <DataTable item={list} />
         </Route>
         <Route exact path="/signup">
           <SignupPage />
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,28 +1,37 @@
 import * as React from 'react';
-import { DataGrid, ColDef } from '@material-ui/data-grid';
 
-const columns: ColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'employee_name', headerName: 'Employee name', width: 200 },
-    { field: 'employee_salary', headerName: 'Salary', width: 130 },
-    {
-        field: 'employee_age',
-        headerName: 'Age',
-        type: 'number',
-        width: 90,
-    },
-];
 
-const rows = [
-    { id: 1, employee_name: 'Jon', employee_age: 35, employee_salary: 50000 },
-];
 
 export default function DataTable(props) {
-    const d = props.list.data;
     return (
-        <div style={{ height: 400, width: '100%' }}>
-            {console.log(d)}
-            <DataGrid rows={d} columns={columns} pageSize={5} checkboxSelection />
+        <div style={{ width: '100%' }}>
+            <table class="table table-sm table-hover table-bordered border-dark">
+                <thead className="table-light">
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Employee</th>
+                        <th scope="col">Salary</th>
+                        <th scope="col">Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        props.item.data && props.item.data.map((s, index) => (
+                            <tr key={s.id} className="todolist">
+
+                                <th scope="row">{s.id}</th>
+                                <td>{s.employee_name}</td>
+                                <td>{s.employee_salary}</td>
+                                <td>{s.employee_age}</td>
+
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+
+
         </div>
     );
 }
+
