3026905f9479b43e2e56484112537b6477c44009
mayankkumawat <mayankkumawat015@gmail.com>
2021-05-06 20:23:09 +0530

    table design

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,36 +1,37 @@
 import * as React from 'react';
 
-
-
 export default function DataTable(props) {
     return (
-        <div style={{ width: '100%' }}>
-            <table class="table table-sm table-hover table-bordered border-dark">
-                <thead className="table-light">
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Employee</th>
-                        <th scope="col">Salary</th>
-                        <th scope="col">Age</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        props.item.data && props.item.data.map((s, index) => (
-                            <tr key={s.id} className="todolist">
-
-                                <th scope="row">{s.id}</th>
-                                <td>{s.employee_name}</td>
-                                <td>{s.employee_salary}</td>
-                                <td>{s.employee_age}</td>
-
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
-
-
+        <div className="row">
+            <div className="container">
+                <div className="col-md-6 offset-md-3">
+                    <div class="table-responsive mt-5">
+                        <table class="table table-sm table-hover caption-top">
+                            {/* <caption className="text-center text-dark -primary lead">List of users</caption> */}
+                            <thead className="table-dark">
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Employee</th>
+                                    <th scope="col">Salary</th>
+                                    <th scope="col">Age</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    props.item.data && props.item.data.map((s, index) => (
+                                        <tr key={s.id} className="table-border table-primary">
+                                            <th scope="row text-center">{s.id}</th>
+                                            <td>{s.employee_name}</td>
+                                            <td>{s.employee_salary}</td>
+                                            <td>{s.employee_age}</td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
